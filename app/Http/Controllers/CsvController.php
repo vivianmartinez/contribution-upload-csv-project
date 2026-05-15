fcd025f33cbef33aba6d91e19f573509812876eb
DESKTOP-1VHAMA3\PTVTELECOM <notiene@gmail.com>
2026-05-15 10:48:01 +0200

    Modificaciónen el método mostrarCsv() para ajustarlo a los cambios de CsvService.

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -23,7 +23,7 @@ class CsvController extends Controller{
      * Recoge el archivo CSV y lo almacena en el servidor.
      *
      * @param  \App\Http\Requests\CsvRequest  $request con el archivo.
-     * @return \Illuminate\Http\RedirectResponse Redirige a la ruta de visualizacion, y devuelve un array con 'archivo' y 'nombreArchivo'.
+     * @return \Illuminate\Http\RedirectResponse Redirige a la ruta de visualizacion, y devuelve un array con 'archivo'.
      */
     public function leerCsv(CsvRequest $request){
    
@@ -31,37 +31,40 @@ class CsvController extends Controller{
 
         $archivoAlmacenado =  $this->csvService->preprocesarCsv($archivo);
 
-        return redirect()->route('mostrar.csv',
-        ['archivo' => $archivoAlmacenado,
+        return redirect()->route('mostrar.csv',[
+            'archivo' => $archivoAlmacenado,
         ]);
     }
 
     /**
      * Coordina la lectura, filtrado y paginación de un archivo CSV para su visualizacion.
      *
-     * @param \Illuminate\Http\Request $request Peticion con la ruta del archivo, terminos de busqueda y opciones de vista.
-     * @return \Illuminate\View\View|\Illuminate\Http\RedirectResponse Vista de visualizacion o redireccion en caso de error.
      */
    public function mostrarCsv(Request $request, $archivo){
+
         if (!Storage::exists($archivo)) { //Si el archivo no existe devuelve error al usuario en el inicio
             return redirect()->route('index')->withErrors('Archivo no encontrado');
         }
 
+        $nombreArchivo= $this->csvService->obtenerNombreArchivo($archivo);
+
         $resultado = $this->csvService->procesarCsv($archivo, $request);
-        if (is_null($resultado) || empty($resultado['columnas'])) {//Si el archivo esta vacio o no tiene cabeceras devuelve error al usuario en el inicio
+        $paginador = $resultado['paginador'];
+        $columnas = $resultado['columnas'];
+
+        if ($paginador->isEmpty()) {//Si el archivo esta vacio o no tiene cabeceras devuelve error al usuario en el inicio
             return redirect()->route('index')->withErrors('El archivo está vacío o es inválido.');
         }
         
         return view('visualizacionCsv', [//Enviamos la informacion a la vista donde se muestra
-            'columnas'       => $resultado['columnas'],
-            'datos'          => $resultado['paginador'],
+            'columnas'       => $columnas,
+            'datos'          => $paginador,
             'archivo'        => $archivo,
-            'nombreArchivo'  => $resultado['nombreOriginal'],
-            'totalFilas'     => $resultado['totalFilas'],
-            'filasPorPagina' => $request->get('opcionesVista', 10)
+            'nombreArchivo'  => $nombreArchivo,
+            'totalFilas'     => $paginador->total(),
+            'filasPorPagina' => $paginador->perPage()
         ]);
     }
-
 
 
     /**
